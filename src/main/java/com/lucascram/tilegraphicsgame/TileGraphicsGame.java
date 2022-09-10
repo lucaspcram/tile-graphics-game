7761c18838b14f007b656f491109f77f8b3b4031
lucaspcram <lucaspcram@gmail.com>
2022-09-10 09:08:53 -0700

    Initial commit

---
+++ src/main/java/com/lucascram/tilegraphicsgame/TileGraphicsGame.java
@@ -0,0 +1,7 @@
+package com.lucascram.tilegraphicsgame;
+
+public class TileGraphicsGame {
+    public static void main(String[] args) {
+        System.out.println("Tile graphics game!");
+    }
+}
